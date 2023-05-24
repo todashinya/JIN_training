6709a0cf71b4eb13fc1a814041484456fc7803cf
H0063\toda <s.toda@jin-it.co.jp>
2023-05-24 14:53:04 +0900

    以下のようにリファクタリングしました ・演算子判定用の関数generateOperatorの作成 ・計算結果と計算式を返却するcalculateの作成

--- ex8/class/QuestionClass.php
+++ ex8/class/QuestionClass.php
@@ -25,66 +25,13 @@ class QuestionClass {
             $a = rand($this->__min, $this->__max);
             $b = rand($this->__min, $this->__max);
 
+            $operator = $this->generateOperator();
+
+            $calculation = $this->calculate($operator, $a, $b);
             
-            // 出題形式のチェック
-            // 出題形式に応じて演算子を決定
-            if($this->__calctype == 1) {
-                $operator = '+';
-            } else if($this->__calctype == 2) {
-                $operator = '-';
-            } else if($this->__calctype == 3) {
-                $operator = '×';
-            } else if($this->__calctype == 4) {
-                $operator = '÷';
-            } else {
-                $array = ['+', '-', '÷', '×'];
-                $arrayIndex = array_rand($array);
-                $operator = $array[$arrayIndex];
-            }
-
-            // 演算子に応じて計算結果を出力
-            switch ($operator) {
-                case '+':
-                    $answer = $a + $b;
-                    break;
-                case '-':
-                    // 条件1:負の解にならない問題作成
-                    if ($a < $b) {
-                        $tmp = $a;
-                        $a = $b;
-                        $b = $tmp;
-                    }
-                    $answer = $a - $b;
-                    // 条件1:END
-                    break;
-                case '×':
-                    $answer = $a * $b;
-                    break;
-                case '÷':
-                    // 条件2:0除算が起きないようにする
-                    if ($a == 0) {
-                        $a = rand(1, $this->__max);
-                    }
-                    // 条件2:END
-
-                    // 条件3:割り算の解が小数点にならないようにする
-                    if ($a % $b != 0) {
-                        $a = floor($a / $b) * $b;
-                    }
-                    // 条件3:END
-
-                    // 条件4:小数第三位を四捨五入して計算
-                    $answer = round(($a / $b), 3);
-                    // 条件4:END   
-                    break;
-            }
-
-            //画面表示用の式を作成
-            $formula = "$a $operator $b";
-
             $this->dataList[] = array(
-                'formula' => $formula,
-                'answer' => $answer,
+                'formula' => $calculation['formula'],
+                'answer' => $calculation['answer'],
                 'display_num' => $i + 1,
             );
 
@@ -92,6 +39,78 @@ class QuestionClass {
         
         return $this->dataList;
     }
+
+    // 出題形式のチェック
+    // 出題形式に応じて演算子を決定
+    private function generateOperator() {
+        if($this->__calctype == 1) {
+            $operator = '+';
+        } else if($this->__calctype == 2) {
+            $operator = '-';
+        } else if($this->__calctype == 3) {
+            $operator = '×';
+        } else if($this->__calctype == 4) {
+            $operator = '÷';
+        } else {
+            $array = ['+', '-', '÷', '×'];
+            $arrayIndex = array_rand($array);
+            $operator = $array[$arrayIndex];
+        }
+        return $operator;
+    }
+
+
+    // 演算子に応じて計算結果を出力
+    private function calculate($operator, $a ,$b) {
+
+        $calculation[] = array();
+
+        switch ($operator) {
+            case '+':
+                $answer = $a + $b;
+                break;
+            case '-':
+                // 条件1:負の解にならない問題作成
+                if ($a < $b) {
+                    $tmp = $a;
+                    $a = $b;
+                    $b = $tmp;
+                }
+                $answer = $a - $b;
+                // 条件1:END
+                break;
+            case '×':
+                $answer = $a * $b;
+                break;
+            case '÷':
+                // 条件2:0除算が起きないようにする
+                if ($a == 0) {
+                    $a = rand(1, $this->__max);
+                }
+                // 条件2:END
+
+                // 条件3:割り算の解が小数点にならないようにする
+                if ($a % $b != 0) {
+                    $a = floor($a / $b) * $b;
+                }
+                // 条件3:END
+
+                // 条件4:小数第三位を四捨五入して計算
+                $answer = round(($a / $b), 3);
+                // 条件4:END   
+                break;
+
+        }
+
+        //画面表示用の式を作成(これが原因ですね)
+        $formula = "$a $operator $b";
+
+        $calculation['formula'] = $formula;
+        $calculation['answer'] = $answer;
+        
+        return $calculation;
+    }
+
 
     /**
      * index.phpからPOSTされた値をもとに、出題形式を返却する
